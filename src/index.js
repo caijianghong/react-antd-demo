15ec4e41099c895b57dcb15aa187ac2b033435f2
蔡江洪 <278436264@qq.com>
2020-10-23 09:24:22 +0800

    update:优化结构

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import {Provider} from 'react-redux'
 import store from './store'
 import {persistor} from './store'
 import {PersistGate} from 'redux-persist/lib/integration/react';
-import Router from './router/index';
+import App from './App';
 import './index.css';
 import 'antd/dist/antd.css'
 ReactDOM.render(
   <Provider store={store}>
      <PersistGate loading={null} persistor={persistor}>
-      <Router />
+      <App />
      </PersistGate>
   </Provider>,
   document.getElementById('root')
